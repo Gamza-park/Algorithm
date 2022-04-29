f08a7375accae38a7806c6068c307216a6600e25
gamza-park <youuy28@gmail.com>
2022-04-29 15:51:38 +0900

    Add Stack

---
+++ python/stack/reverseString.py
@@ -0,0 +1,19 @@
+from listStack import *
+
+def reverse(str):
+    st = ListStack()
+    for i in range(len(str)):
+        st.push(str[i])
+    out = ""
+    while not st.isEmpty():
+        out += st.pop()
+    return out
+
+def main():
+    input = "Test Seq 12345"
+    answer = reverse(input)
+    print("Input string: ", input)
+    print("Reverse string: ", answer)
+
+if __name__ == "__main__":
+    main()
